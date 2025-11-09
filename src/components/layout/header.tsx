bf303f09522b5db290e14397431b310186ccfe26
RodrigoRafaelSantos7 <rodrigorafaelsantos7@icloud.com>
2025-11-09 16:20:38 +0100

    Add tooltip functionality to SidebarTrigger in Header component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,12 @@
 import { ThemeSelector } from "@/components/app/theme-selector";
 import { UserMenu } from "@/components/app/user-menu";
+import { Kbd, KbdGroup } from "@/components/ui/kbd";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 
 export function Header() {
@@ -11,7 +17,18 @@ export function Header() {
       )}
     >
       <div className="flex items-center gap-2 p-3">
-        <SidebarTrigger />
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <SidebarTrigger />
+          </TooltipTrigger>
+          <TooltipContent>
+            <KbdGroup>
+              <Kbd>Ctrl</Kbd>
+              <span>+</span>
+              <Kbd>B</Kbd>
+            </KbdGroup>
+          </TooltipContent>
+        </Tooltip>
       </div>
       <div className="flex items-center gap-2 p-3">
         <ThemeSelector />
