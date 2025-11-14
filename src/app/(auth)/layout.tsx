95b157f28d8bd7ca1837ff3977f15b2d10602eaa
RodrigoRafaelSantos7 <rodrigorafaelsantos7@icloud.com>
2025-11-14 15:12:16 +0100

    Add authentication layout and login page components</message>
    <message>Created a new `layout.tsx` file for authentication that handles token retrieval and redirects authenticated users to the index path. Added a `login/page.tsx` file for the login page component. Introduced `paths.ts` to define the index path for navigation, enhancing the authentication flow and structure of the application.

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { getToken } from "@/lib/auth/auth-server";
+import { indexPath } from "@/lib/paths";
+
+export const metadata: Metadata = {
+  title: "Welcome Back",
+  description:
+    "Get access to AI models from Mistral. Unlimited tier is free for everyone!",
+};
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const token = await getToken();
+
+  if (token) {
+    redirect(indexPath());
+  }
+
+  return (
+    <div className="flex h-full min-h-screen min-w-screen flex-col items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
